4fca39c6df07af7525c0bd199f05164345003abf
Sai Charan Uvaraj <saicharanuvi01@gmail.com>
2024-06-26 15:08:56 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,22 @@
-/*
 const date=new Date();
 let year=date.getFullYear();
 let months={
-    0:"Jan", 1:"Feb", 2:"Mar", 3:"Apr", 4:"May", 5:"Jun", 6:"Jul", 7:"Aug", 8:"Sep", 9:"Oct", 10:"Nov", 11:"Dec"
+    0:"January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"
 }
 let month=months[date.getMonth()];
+let days={
+    0:"Sunday", 1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"
+}
+let day=days[date.getDay()];
 let dn=date.getDate();
-*/
 function Footer()
 {
     return(
-        <footer className="footer">
+        <footer className="fixed bottom-0 left-0 right-0 text-white bg-[#31363F] text-sm shadow-2xl shadow-black">
+            <div className="grid place-items-center p-1">
+                <p>Keep Your Notes using Keeper</p>
+                <p>{day+", "+month+" "+dn+", "+year}</p>
+            </div>
         </footer>
     )
 }
